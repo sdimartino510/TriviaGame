a0fcab0f401959a4436cc7051fd0cfeb76726dac
ArchangelD <sdimartino@gmail.com>
2019-01-12 17:28:10 -0800

    Begins adding game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,17 @@
-$("document").ready({
+$(document).ready(function() {
 
-//"start game" button sets the game up: starts timer at 2 minutes (120 sec), and populates 10 questions with 4 multiple choice answers each.
+    //"start game" button sets the game up: starts timer at 2 minutes (120 sec), and populates 10 questions with 4 multiple choice answers each, and a submit button.
 
-//
+    $(document).on("click", ".button", function() {
+        console.log("click");
+        
+    })
+
+    //Each multiple choice option will have a radio button to select it. Only one option can be selected.
+
+    //Submit button will check answers and display number correct, number incorrect, and number unanswered.
+
+    //If game timer reaches zero, call submit button function (end game).
 
 
 })
