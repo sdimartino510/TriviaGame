25da5e04315baad7833c0afdbeefb0fe7d9efec1
ArchangelD <sdimartino@gmail.com>
2019-01-16 01:27:37 -0800

    Begins adding game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,44 +27,6 @@ $(document).ready(function() {
       
       }
 
-    //   function timeConverter(t) {
-
-    //     //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-    //     var minutes = Math.floor(t / 60);
-    //     var seconds = t - (minutes * 60);
-      
-    //     if (seconds < 10) {
-    //       seconds = "0" + seconds;
-    //     }
-      
-    //     if (minutes === 0) {
-    //       minutes = "00";
-    //     }
-      
-    //     else if (minutes < 10) {
-    //       minutes = "0" + minutes;
-    //     }
-      
-    //     return minutes + ":" + seconds;
-    //   }
-
-    //   function count() {
-
-    //     //  Decrement time by 1.
-      
-    //     time--;
-      
-        //  TODO: Get the current time, pass that into the timeConverter function,
-        //        and save the result in a variable.
-      
-        // const convertedTime = timeConverter(time);
-      
-        //  TODO: Use the variable you just created to show the converted time in the "display" div.
-      
-        // $("#display").text(convertedTime);
-      
-      
-
       console.log(time);
 
     //Each multiple choice option will have a radio button to select it. Only one option can be selected.
