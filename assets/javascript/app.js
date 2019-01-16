40565cfc884fc462513e456c1253742f5ce641cd
ArchangelD <sdimartino@gmail.com>
2019-01-15 16:12:05 -0800

    Finishes styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 
     //"start game" button sets the game up: starts timer at 2 minutes (120 sec), and populates 10 questions with 4 multiple choice answers each, and a submit button.
 
-    $(document).on("click", ".button", function() {
+    $(document).on("click", ".start-button", function() {
         console.log("click");
         $(".hidden-box").css("display", "block");
-        $(".button").css("display", "none");
+        $(".start-button").css("display", "none");
         
     })
 
